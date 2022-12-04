00b9fd9fa6a705f92ca9d62acb4754b66c160cdd
Mohamed A <mohamed.abdul.dimbio@gmail.com>
2022-12-03 23:14:21 -0500

    Added the UCS algorithm

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
-from board import Board
+# This is a sample Python script.
+from A import SolveA
+from GBFS import SolveGBFS
+from UCS import SolveUCS
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def board():
-    board_obj = Board()
-    Board.list_of_boards(board_obj)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    F = open("sample-input.txt", "r")
 
+    Data = F.read()
 
-if __name__ == "__main__":
-    board()
+    Data = Data.split("\n")
+
+    count = 1
+    #  UCS
+    for S in Data:
+        if len(S) == 0:
+            continue
+        if S[0] == '#':
+            continue
+        SolveUCS(S, count)
+        count += 1
+    #  GBFS
+    count = 1
+    for S in Data:
+        if len(S) == 0:
+            continue
+        if S[0] == '#':
+            continue
+        for method in range(1, 5):
+            SolveGBFS(S, count, method)
+        count += 1
+    # A
+    count = 1
+    for S in Data:
+        if len(S) == 0:
+            continue
+        if S[0] == '#':
+            continue
+        for method in range(1, 5):
+            SolveA(S, count, method)
+        count += 1
